98bb78224c21e4e9fec782ad876ae187a7ab808a
JuanLP <juan.luis.palacios.p@gmail.com>
2022-04-28 00:59:53 -0500

    Improve readabily of animas sample data

--- data.sql
+++ data.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO animals
     (name,      date_of_birth,  weight_kg,  neutered,   escape_attempts ) VALUES 
-	('Agumon',  '2020-02-03',   10.23,      B'1',       0               ),
-    ('Gabumon', '2018-11-15',   8,          B'1',       2               ),
-    ('Pikachu', '2021-01-07',   15.04,      B'0',       1               ),
-    ('Devimon', '2017-05-12',   11,         B'1',       5               ); 
+	('Agumon',  '2020-02-03',   10.23,      TRUE,       0               ),
+    ('Gabumon', '2018-11-15',   8,          TRUE,       2               ),
+    ('Pikachu', '2021-01-07',   15.04,      FALSE,      1               ),
+    ('Devimon', '2017-05-12',   11,         TRUE,       5               ); 
